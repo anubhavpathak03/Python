8c108c021abf8bfa4f526b53871e6db93c710dee
anubhavpathak03 <pathak.anubhavdtu@gmail.com>
2025-06-02 00:22:10 +0530

    learn NumPy

--- Practice/01_practice.py
+++ Practice/01_practice.py
@@ -1,4 +1,13 @@
-nums = [3, 0, 1, 4]
+#  list
+nums = [3, 0, 1, 4] 
 
 for i, v in enumerate(nums):
     print(i, v)
+
+print();
+
+for index, num in enumerate(nums):
+    print(f"Index {index} => {num} ");
+
+
+print(list(enumerate(nums)));
